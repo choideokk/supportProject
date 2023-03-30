0f89e49d1f1c32696d8cde7290157d64b8abd57c
choideokk <ejrgml1107@naver.com>
2023-03-30 15:06:32 +0900

    메인 이벤트블럭 추가, 비디오 캐러셀, 모바일 메뉴버튼 수정

---
+++ js/common.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".menuBtn");
+const menuList = document.querySelector(".topMenu");
+
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('isVisible');
+    menuBtn.classList.toggle('isCross');
+})
--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
-const menuBtn = document.querySelector("#menuBtn");
-const menuList = document.querySelector(".topMenu");
+let count = 0;
+const vidTxtArr = ["동물들을 위한 캠페인", "미래세대를 위한 지구를 지키는 캠페인"];
 
-menuBtn.addEventListener('click', () => {
-    menuList.classList.toggle('isVisible');
+const vidPlayer = document.querySelector(".vidPlayer video");
+const vidTxt = document.querySelector(".vidPlayer .vidTxt");
+const vidCarousel = document.querySelector(".vidPlayer .vidCarousel");
+const prevBtn = document.querySelector(".vidPlayer .prevBtn");
+const nextBtn = document.querySelector(".vidPlayer .nextBtn");
+
+prevBtn.addEventListener("click", () => {
+    prevBtn.classList.toggle("isHidden");
+    vidTxt.textContent = vidTxtArr[0];
+    vidCarousel.style.transform = "translateX(0%)";
+    nextBtn.classList.toggle("isHidden");
+})
+
+
+nextBtn.addEventListener("click", () => {
+    prevBtn.classList.toggle("isHidden");
+    vidTxt.textContent = vidTxtArr[1];
+    vidCarousel.style.transform = "translateX(-100%)";
+    nextBtn.classList.toggle("isHidden");
 })
