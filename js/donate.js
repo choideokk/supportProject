e42f4a10b140ba8db9de526a3fd5b6b69f9ba3ad
choideokk <ejrgml1107@naver.com>
2023-04-03 12:28:00 +0900

    후원 폼 기능 추가, 제출 후 메인 페이지로 연결

--- js/donate.js
+++ js/donate.js
@@ -6,9 +6,7 @@ const progressLine = document.querySelector(".progressBar .line");
 
 const directMoneyInp = document.querySelector("#moneyInp");
 const totalMoneyInp = document.querySelector(".totalCost input");
-const firstInfo = document.querySelector(".firstInfoWrapper");
-const firstH3 = document.querySelector(".firstH3");
-const secondH3 = document.querySelector(".secondH3");
+const selectInp = document.querySelector(".dnList");
 
 const regularBtn = document.querySelector(".regularSend");
 const onceBtn = document.querySelector(".onceSend");
@@ -16,33 +14,49 @@ const goNextBtn = document.querySelector(".goNextBtn");
 const goBackBtn = document.querySelector(".goBackBtn");
 
 const withdrawTxt = document.querySelector(".withdraw");
+const firstInfoWrapper = document.querySelector(".firstInfoWrapper");
+const firstInfo = document.querySelector(".firstInfoWrapper p");
+const firstH3 = document.querySelector(".firstH3");
+const secondH3 = document.querySelector(".secondH3");
 const moneyLabel = document.querySelectorAll(".moneyWrapper label input[type='radio']");
+const warningTxt1 = document.querySelector(".warningActTxt");
+const warningTxt2 = document.querySelector(".warningMoneyTxt");
 
 window.onload = () => {
     for (let i = 0; i < moneyLabel.length; i++) {
         moneyLabel[i].addEventListener("click", () => {
-            totalMoneyInp.value = Number(moneyLabel[i].value);
+            totalMoneyInp.value = Number(moneyLabel[i].value).toLocaleString();
+            directMoneyInp.value = "";
+            warningTxt2.classList.add("isHidden");
         })
     }
 }
 
 
-directMoneyInp.addEventListener("focus", function () {
+directMoneyInp.addEventListener("keyup", () => {
+    if (directMoneyInp.value.length <= 7) {
+        totalMoneyInp.value = Number(directMoneyInp.value).toLocaleString();
+    } else {
+        directMoneyInp.value = Number(directMoneyInp.value.substr(0, 7));
+        warningTxt2.textContent = "최대 후원 금액은 1,000,000 미만입니다."
+        warningTxt2.classList.remove("isHidden");
+    }
+})
+
+selectInp.addEventListener("blur", () => {
+    if (selectInp.value === "") {
+        warningTxt1.classList.remove("isHidden");
+    } else {
+        warningTxt1.classList.add("isHidden");
+    }
+})
+
+directMoneyInp.addEventListener("focus", () => {
     directMoneyInp.value = "";
+    totalMoneyInp.value = "0";
     for (insideRadioBtn of moneyLabel) {
         insideRadioBtn.checked = false;
     }
-});
-
-directMoneyInp.addEventListener("blur", () => {
-    if (totalMoneyInp.value.trim() === "") return;
-
-    for (mBtn of moneyLabel) {
-        const insideRadioBtn = mBtn.querySelector("input[type='radio']");
-    }
-
-    totalMoneyInp.value = Number(directMoneyInp.value).toLocaleString();
-    directMoneyInp.value = totalMoneyInp.value;
 });
 
 regularBtn.addEventListener("click", () => {
@@ -54,9 +68,31 @@ onceBtn.addEventListener("click", () => {
 })
 
 goNextBtn.addEventListener("click", () => {
+    if (document.donate.actsList.value === "") {
+        warningTxt1.classList.remove("isHidden");
+        return;
+    };
+    if ((document.donate.money.value === "") && (document.donate.moneyInput.value === "")) {
+        warningTxt2.textContent = "최소 후원 금액은 1,000원 이상입니다.";
+        warningTxt2.classList.remove("isHidden");
+        return;
+    }
+
+    const donateType = document.donate.sendType.value;
+    const donateMoney = document.donate.money.value;
+    const donateInp = document.donate.moneyInput.value;
+    const donateAct = document.donate.actsList.value;
+
+    if (donateMoney === "") {
+        firstInfo.textContent = `${donateType} / ${Number(donateInp).toLocaleString()} / ${donateAct}`;
+    } else {
+        firstInfo.textContent = `${donateType} / ${Number(donateMoney).toLocaleString()} / ${donateAct}`;
+    }
+
     firstH3.classList.add("isMuted");
     secondH3.classList.remove("isHidden");
-    firstInfo.style.display = "flex";
+    firstInfoWrapper.style.display = "flex";
+    firstInfoWrapper.classList.toggle("isHidden");
     leftSide.classList.toggle("isHidden");
     rightSide.classList.toggle("isHidden");
     progressCircle1.classList.toggle("isCompleted");
@@ -67,7 +103,7 @@ goNextBtn.addEventListener("click", () => {
 goBackBtn.addEventListener("click", () => {
     firstH3.classList.remove("isMuted");
     secondH3.classList.add("isHidden");
-    firstInfo.style.display = "none";
+    firstInfoWrapper.style.display = "none";
     leftSide.classList.toggle("isHidden");
     rightSide.classList.toggle("isHidden");
     progressCircle1.classList.toggle("isCompleted");
---
+++ js/donate2.js
@@ -0,0 +1,30 @@
+
+const totalForm = document.donate;
+const warningTxts = document.querySelectorAll(".rightSide .warningTxt");
+const rightSideInputs = document.querySelectorAll(".rightSide input[type='text']");
+
+for (let i = 0; i < rightSideInputs.length; i++) {
+    rightSideInputs[i].addEventListener("blur", () => {
+        if (rightSideInputs[i].value === "") {
+            warningTxts[i].classList.remove("isHidden");
+        } else {
+            warningTxts[i].classList.add("isHidden");
+        }
+    })
+}
+totalForm.addEventListener("submit", (e) => {
+    let isAllChecked = false;
+    e.preventDefault();
+    const donateAccount = document.donate.account.value;
+    const donateMaster = document.donate.master.value;
+    const donateBirth = document.donate.birth.value;
+    const donateCheck = document.querySelectorAll(".rightSide input[type='checkbox']");
+    isAllChecked = donateCheck[0].checked && donateCheck[1].checked;
+
+    if (donateAccount === "" || donateMaster === "") return;
+
+    if (donateBirth === "" || (!isAllChecked)) return;
+
+    alert("후원해주셔서 감사합니다.");
+    window.location.href = "http://127.0.0.1:5500/supportProject/index.html";
+})
